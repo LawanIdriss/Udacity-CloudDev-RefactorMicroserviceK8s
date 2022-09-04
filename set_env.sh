5bba7e386f15e81a6716b871dc565c3fd7d66319
Lawan Idriss <37778249+LawanIdriss@users.noreply.github.com>
2022-09-04 13:42:57 +0100

    general project update

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=database-1.c6ogqqa7hc81.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=bucket-web
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
